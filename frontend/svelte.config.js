3fb8d3c23c072e99faffd0523bce495673af1661
seanbetts <seanbetts@icloud.com>
2026-01-01 20:29:43 +0000

    Fix Vercel build adapter and build script

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const adapter = process.env.VERCEL
+	? (await import('@sveltejs/adapter-vercel')).default
+	: (await import('@sveltejs/adapter-node')).default;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
