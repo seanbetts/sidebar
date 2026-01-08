c6eca491f59f198baf40c2ee3a0bf5a16a8303e2
seanbetts <seanbetts@icloud.com>
2026-01-08 21:18:28 +0000

    ios: add Xcode project scaffolding

---
+++ ios/sideBar/sideBar/sideBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  sideBarApp.swift
+//  sideBar
+//
+//  Created by Sean Betts on 08/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct sideBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/sideBar/sideBarTests/sideBarTests.swift
@@ -0,0 +1,16 @@
+//
+//  sideBarTests.swift
+//  sideBarTests
+//
+//  Created by Sean Betts on 08/01/2026.
+//
+
+import Testing
+
+struct sideBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
