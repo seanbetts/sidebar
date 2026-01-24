13cfbf43cd83666c1f0858689b6211cd724a23da
seanbetts <seanbetts@icloud.com>
2026-01-24 22:43:45 +0000

    Fix UITest lint violations

--- ios/sideBar/sideBarUITests/sideBarUITestsLaunchTests.swift
+++ ios/sideBar/sideBarUITests/sideBarUITestsLaunchTests.swift
@@ -7,9 +7,9 @@
 
 import XCTest
 
-final class sideBarUITestsLaunchTests: XCTestCase {
+final class SideBarUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+    override static var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
 
