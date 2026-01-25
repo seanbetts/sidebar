fa7ae1634599e1ce869c99c9ca0a8997a6b5d60f
seanbetts <seanbetts@icloud.com>
2026-01-25 10:48:17 +0000

    Remove custom block menu from keyboard toolbar

--- ios/sideBar/sideBar/Views/Notes/NativeMarkdownEditorView.swift
+++ ios/sideBar/sideBar/Views/Notes/NativeMarkdownEditorView.swift
@@ -35,29 +35,5 @@ public struct NativeMarkdownEditorView: View {
                 }
         }
         .background(DesignTokens.Colors.background)
-        #if os(iOS)
-        .toolbar {
-            ToolbarItemGroup(placement: .keyboard) {
-                blockMenu
-            }
-        }
-        #endif
-    }
-
-    private var blockMenu: some View {
-        Menu {
-            Button("Heading 1") { viewModel.applyHeading(level: 1) }
-            Button("Heading 2") { viewModel.applyHeading(level: 2) }
-            Button("Heading 3") { viewModel.applyHeading(level: 3) }
-            Divider()
-            Button("Bulleted list") { viewModel.applyList(ordered: false) }
-            Button("Numbered list") { viewModel.applyList(ordered: true) }
-            Button("Task") { viewModel.applyTask() }
-            Divider()
-            Button("Quote") { viewModel.applyQuote() }
-            Button("Code block") { viewModel.applyCodeBlock(language: nil) }
-        } label: {
-            Image(systemName: "textformat")
-        }
     }
 }
