5d214b161b7a894461924c5f2946b12ca7f31b5c
seanbetts <seanbetts@icloud.com>
2026-01-25 18:07:19 +0000

    Import platform UI for NSParagraphStyle

--- ios/sideBar/sideBar/Utilities/Concurrency/NSParagraphStyle+Sendable.swift
+++ ios/sideBar/sideBar/Utilities/Concurrency/NSParagraphStyle+Sendable.swift
@@ -1,3 +1,8 @@
 @preconcurrency import Foundation
+#if canImport(UIKit)
+@preconcurrency import UIKit
+#elseif canImport(AppKit)
+@preconcurrency import AppKit
+#endif
 
 extension NSParagraphStyle: @unchecked Sendable {}
